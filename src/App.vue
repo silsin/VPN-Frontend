248c7f098635d9d56db36f3ef23e5a1ce444dc4c
hasan <hasan@digilite.ca>
2026-02-01 14:19:19 +0330

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,3 @@
   <router-view />
 </template>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: 'Vazirmatn', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-  direction: rtl;
-}
-</style>
